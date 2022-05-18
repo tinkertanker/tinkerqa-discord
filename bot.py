8b9ca900891d60eee8ab7dd91e5f0080d769ab0c
Robert <xiurobert@gmail.com>
2022-05-18 13:51:03 +0800

    feat: Get mkthread command working

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Option
 from dotenv import load_dotenv
 import os
 
@@ -8,15 +9,24 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Bot(intents=intents)
 
+guilds = [976345115826212884]
+qa_channel = 976356366316875807
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 
-@bot.slash_command(guild_ids=[976345115826212884])
-async def test(ctx):
-    await ctx.respond("Hello World!")
+@bot.slash_command(guild_ids=guilds, name="mkthread",
+                   description="Creates a new thread in the #qa channel")
+async def create(ctx: discord.commands.context.ApplicationContext,
+                 question: Option(str, "What is your question?", required=True, default='')):
+    if not question:
+        await ctx.respond("Please try again, with an ACTUAL question")
+        return
+    await ctx.respond(f"Your question is: {question}. "
+                      f"I have created a new thread for you in {bot.get_channel(qa_channel).mention}")
+    await bot.get_channel(qa_channel).send(question)
 
 
 if __name__ == "__main__":
