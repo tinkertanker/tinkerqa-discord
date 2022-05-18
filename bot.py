c76cdd140a487504c93d187d30a6497a5953787a
Robert <xiurobert@gmail.com>
2022-05-18 12:36:44 +0800

    feat: initial prototype of bot

--- bot.py
+++ bot.py
@@ -1,3 +1,24 @@
-from discord.ext import commands
+import discord
+from dotenv import load_dotenv
+import os
 
-bot = commands.Bot
+load_dotenv()
+
+bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.slash_command(guild_ids=[''])
+async def slash_command(ctx):
+    await ctx.respond("Hello World!")
+
+
+if __name__ == "__main__":
+    bot.run(os.environ.get('DISCORD_TOKEN'))
