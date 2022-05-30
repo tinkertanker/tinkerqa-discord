1cc395a18af0763121e6f3a583fbf30127cc52b5
Robert <xiurobert@gmail.com>
2022-05-30 11:49:46 +0800

    bugfix: fix potential bug with ids

--- bot.py
+++ bot.py
@@ -6,8 +6,10 @@ from tinkerqa_discord import TinkerQaDiscord, Config
 
 if __name__ == "__main__":
     load_dotenv()
-    cfg = Config(guild=os.getenv("GUILD_QA_CHANNEL"),
-                 qa_channel=os.getenv("GUILD_QA_CHANNEL"),
-                 helper_role=os.getenv("GUILD_HELPER_ROLE"))
+    cfg = Config(
+        guild=int(os.getenv("GUILD_QA_CHANNEL")),
+        qa_channel=int(os.getenv("GUILD_QA_CHANNEL")),
+        helper_role=int(os.getenv("GUILD_HELPER_ROLE"))
+    )
     bot = TinkerQaDiscord(cfg)
-    bot.run(os.environ.get('DISCORD_TOKEN'))
+    bot.run(os.getenv('DISCORD_TOKEN'))
